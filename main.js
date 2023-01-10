65812b6e857f346013b05e03a8b5a389d04d5f00
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-01-10 21:52:42 +0100

    cal

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 // selector
-// const calDisplay = document.querySelector('.cal_display')
-// const calKey = document.querySelector('.cal_keys')
+const wrapperEl = document.querySelector('.wrapper')
+const calDisplay = document.querySelector('.cal_display')
+const calKey = document.querySelector('.cal_keys')
 
+keys.addEventListener(
+    'click',
+    (e) => {
+        if(e.target.matches('button')){
+            const key = e.target
+            const action = key.dataset.action
+            const keyContent = key.textContent
+            const displayedNum = display.textContent
+        }
+    }
+)
